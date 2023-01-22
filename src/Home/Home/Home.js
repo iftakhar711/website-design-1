caf2edd1f68abbd9b5207190d547f8a42f2db08c
iftakhar711 <iftakharudiin80@gmail.com>
2023-01-22 11:16:12 +0600

    first commit

---
+++ src/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../Components/Navbar/Nav';
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+        </div>
+    );
+};
+
+export default Home;
